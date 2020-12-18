53b27c5254017cda4d0a74f240854ae2c8e9c26a
Jacek Chmielewski <jchmielewski@teonite.com>
2020-12-18 13:33:21 +0100

    Cleanup, fixed u8_to_f32.

--- src/sdr.rs
+++ src/sdr.rs
@@ -1,14 +1,11 @@
-use std::thread;
 use std::sync::{Arc, Mutex};
 use std::f32::consts::PI;
-use cpal::{Data, Sample, SampleFormat};
+use cpal::Sample;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use crate::error::RustFmError;
-use std::convert::TryInto;
 
 pub fn u8_to_f32(input: &[u8]) -> Vec<f32> {
-    input.iter().map(|e| (*e as f32) / (std::u8::MAX as f32)).collect()
-    // input.try_into().unwrap()
+    input.iter().map(|e| *e as f32 * 2. / (std::u8::MAX as f32) - 1.).collect()
 }
 
 pub struct DownSampler {
@@ -89,66 +86,22 @@ impl AudioPlayer {
     }
 
     pub fn start(&mut self) -> cpal::Stream {
-        // let host = cpal::default_host();
-        // let device = host.default_output_device().expect("no output device available");
-        // let mut supported_configs_range = device.supported_output_configs()
-        //     .expect("error while querying configs");
-        // let supported_config = supported_configs_range.next()
-        //     .expect("no supported config?!")
-        //     .with_max_sample_rate();
-        // let err_fn = |err| eprintln!("an error occurred on the output audio stream: {}", err);
-        // // let sample_format = supported_config.sample_format();
-        // let config = supported_config.into();
-        // // let stream = match sample_format {
-        //     // SampleFormat::F32 => device.build_output_stream(&config, write_silence::<f32>, err_fn),
-        //     // SampleFormat::I16 => device.build_output_stream(&config, write_silence::<i16>, err_fn),
-        //     // SampleFormat::U16 => device.build_output_stream(&config, write_silence::<u16>, err_fn),
-
-        // //     SampleFormat::F32 => println!("F32"),
-        // //     SampleFormat::I16 => println!("I16"),
-        // //     SampleFormat::U16 => println!("U16"),
-        // // };
-        // // }.unwrap();
-        // // stream.play().unwrap();
-
-        // let stream = device.build_output_stream(&config, write_silence, err_fn).unwrap();
-        // stream.play().unwrap();
-        // sleep(time::Duration::from_secs(2));
-        // use cpal::traits::HostTrait;
-
         let host = cpal::default_host();
-
         let device = host.default_output_device().expect("no output device available");
-
-        // use cpal::traits::{DeviceTrait, HostTrait};
         let mut supported_configs_range = device.supported_output_configs()
             .expect("error while querying configs");
         let supported_config = supported_configs_range.next()
             .expect("no supported config?!")
             .with_max_sample_rate();
 
-            // use cpal::Data;
-        // use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-        // let stream = device.build_output_stream(
-        //     &config,
-        //     move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-        //         // react to stream events and read or write stream data here.
-        //     },
-        //     move |err| {
-        //         // react to errors here.
-        //     },
-        // );
-
         let err_fn = |err| eprintln!("an error occurred on the output audio stream: {}", err);
         let sample_format = supported_config.sample_format();
         let config = supported_config.into();
 
         let buffer = Arc::clone(&self.buffer);
-        // std::thread::spawn(move || {
             
             let write_silence = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
                 let mut buffer = buffer.lock().unwrap();
-                // println!("Writing silence");
                 for sample in data.iter_mut() {
                     if let Some(s) = buffer.get_mut(0) {
                         *sample = Sample::from(s);
@@ -158,24 +111,10 @@ impl AudioPlayer {
                         break;
                     }
                 }
-                // println!("#");
             };
-
-            // fn write_silence<T: Sample>(data: &mut [T], _: &cpal::OutputCallbackInfo) {
-            //     for sample in data.iter_mut() {
-            //         *sample = Sample::from(self.buffer[0]);
-            //     }
-            // }
-
-            // let stream = match sample_format {
-            //     SampleFormat::F32 => device.build_output_stream(&config, write_silence::<f32>, err_fn),
-            //     SampleFormat::I16 => device.build_output_stream(&config, write_silence::<i16>, err_fn),
-            //     SampleFormat::U16 => device.build_output_stream(&config, write_silence::<u16>, err_fn),
-            // }.unwrap();
             
             let stream = device.build_output_stream(&config, write_silence, err_fn).unwrap();
             stream
-        // });
     }
 
     fn play(&self, input: &mut Vec<f32>) {
@@ -189,22 +128,26 @@ pub fn fm_play(freq: u32) -> Result<(), RustFmError> {
     let (mut ctl, mut reader) = rtlsdr_mt::open(0)?;
 
     ctl.enable_agc()?;
-    // ctl.set_ppm(-2)?;
-    // ctl.set_center_freq(freq + 5000)?;
     ctl.set_center_freq(freq)?;
     ctl.set_sample_rate(1102500)?;
 
     let mut downsampler = DownSampler::new(25);
-    let mut demodulator = FMDemodulator::new(44_100., 1., 75_000.);
+    // let mut demodulator = FMDemodulator::new(44_100., 1., 75_000.);
+    let mut demodulator = FMDemodulator::new(1102500., 1., 75_000.);
     let mut player = AudioPlayer::new();
     let stream = player.start();
     stream.play().unwrap();
     reader.read_async(4, 32768, |bytes| {
-        // let demodulated = demodulator.demod(&u8_to_f32(bytes));
-        // let mut downsampled = downsampler.downsample(&demodulated);
-        let mut downsampled = downsampler.downsample(&u8_to_f32(bytes));
-        let mut demodulated = demodulator.demod(&downsampled);
-        player.play(&mut demodulated);
+        let bytes_float = u8_to_f32(bytes);
+        let mut demodulated = demodulator.demod(&bytes_float);
+        let mut downsampled = downsampler.downsample(&demodulated);
+        player.play(&mut downsampled);
     })?;
     Ok(())
+}
+
+#[test]
+fn test_u8_to_f32() {
+    let data: &[u8] = &[0, std::u8::MAX];
+    assert_eq!(u8_to_f32(data), vec![-1., 1.])
 }
